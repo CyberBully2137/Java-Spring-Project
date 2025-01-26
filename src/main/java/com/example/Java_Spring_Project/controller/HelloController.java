9f6c7e728f58e60a46fccc0c11720c6486e78e9e
Patryk Kotnis <kotnis_p@tlen.pl>
2025-01-26 15:45:22 +0100

    Zadanie1

--- src/main/java/com/example/Java_Spring_Project/controller/HelloController.java
+++ src/main/java/com/example/Java_Spring_Project/controller/HelloController.java
@@ -1,21 +1,23 @@
 package com.example.Java_Spring_Project.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class HelloController {
 
     @GetMapping(value = "/")
-    public String hello(){
+    public String hello() {
         return "Hello Vistula, in my first Spring Controller.";
     }
+
     @GetMapping("/greeting")
-    public String greeing(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
     }
-    //http:/localhost:8080/greeting?name=Vistula
+
+    // http://localhost:8080/greeting?name=Vistula
 }
